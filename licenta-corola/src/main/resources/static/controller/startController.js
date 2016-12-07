a88934b3f979161bea45f25aeef75ac6c039b0f5
p3700664 <p3700664@ness.com>
2016-12-07 18:44:33 +0200

    ready on start moved

--- licenta-corola/src/main/resources/static/controller/startController.js
+++ licenta-corola/src/main/resources/static/controller/startController.js
@@ -6,6 +6,12 @@ app.controller("startController", function ($scope, $http) {
         }
 
     $scope.statProcess = function(){
+
+        //o idee de validare pe start
+        if(document.getElementById("textFileToUpload").value != null)
+            console.log(document.getElementById("textFileToUpload").value);
+
+
          $http.post("/start").success(function (data) {
             })
     }
